7d52e94a19c33132eaff12c7d20372065daa5d07
Colin Schlecht <schlechtcolin@yahoo.com>
2021-02-22 00:27:20 -0800

    commit tree 7ba776611740503b17e79a5b55301c27ab5f5d81

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(cards, event) {
+    let arr = []
+    for (let i = 0; i < cards.length; i ++) {
+        arr.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+    return arr
+}
+
+function countDown(n) {
+    while (n > -1) {
+        console.log(n);
+        n--;
+    }
+}
